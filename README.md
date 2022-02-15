738c1079884f140834a4064714b7efc631806492
Akhil <akhillav@gmail.com>
2022-02-15 22:50:13 +0530

    added readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# SpaceLearn
+A glorified todo-list. Create spaced learning cards for your tasks!
+
+### Features:
+1. Create spaced learning cards for upto 4 days
+
+### Upcoming:
+1. UI/UX revamp
+2. Desktop and mobile notifications
--- server/server.js
+++ server/server.js
@@ -6,8 +6,9 @@ const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const SQLiteStore = require("connect-sqlite3")(session);
-
 const database = require("./db/db");
+
+const router = express.Router();
 
 const { db } = database;
 
@@ -27,7 +28,7 @@ app.use(
 
 app.use(
   session({
-    store: new SQLiteStore,
+    store: new SQLiteStore(),
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
@@ -40,105 +41,108 @@ app.use(passport.initialize());
 app.use(passport.session());
 require("./passportConfig")(passport);
 
-// Routes
+// Login and register -> authentication
 app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user) => {
-    if (err) throw err
+  passport.authenticate("local", (error, user) => {
+    if (error) throw error;
     if (!user) {
-      res.send({ message: "No user", status: 406 }) // 406 = unacceptable information
+      res.send({ message: "No user", status: 406 }); // 406 = unacceptable information
+    } else {
+      req.logIn(user, (error) => {
+        if (error) throw error;
+        res.send({ message: "Logged in", status: 200 });
+      });
     }
-    else {
-      req.logIn(user, (err) => {
-        if (err) throw err
-        res.send({ message: "Logged in", status: 200 })
-      })
-    }
-  })
-    (req, res, next)
-})
+  })(req, res, next);
+});
 
 app.post("/register", async (req, res) => {
-  const encryptedPassword = await bcrypt.hash(req.body.password, 10)
+  const encryptedPassword = await bcrypt.hash(req.body.password, 10);
 
-  db.get(`SELECT * FROM users WHERE username = ?`, [req.body.username], (err, row) => {
+  db.get("SELECT * FROM users WHERE username = ?", [req.body.username], (error, row) => {
     if (row) {
-      res.send({ message: "Exists", status: 409 }) // conflict with current state since user exists
+      res.send({ message: "Exists", status: 409 }); // conflict with current state since user exists
     } else {
-      db.run("Insert INTO users (username, password) values (?, ?)", [req.body.username, encryptedPassword], (err) => {
-        if (err) {
-          console.log(err)
+      db.run("Insert INTO users (username, password) values (?, ?)", [req.body.username, encryptedPassword], (error) => {
+        if (error) {
+          console.log(error);
         }
-        res.send({ message: "Added", status: 200 })
-      })
+        res.send({ message: "Added", status: 200 });
+      });
     }
-  })
-})
+  });
+});
+
+// Cards get their own route
 
 app.post("/addCard", (req, res) => {
-  if (!req.user) return res.send("Please log in")
+  if (!req.user) return res.send("Please log in");
 
-  db.run("INSERT INTO cards (user_id, card_name, card_date) VALUES (?, ?, ?)", [req.user.id, req.body.cardName, req.body.cardDate], (err) => {
-    if (err) console.log(err)
-    console.log("Card added")
-    db.all("SELECT card_id, card_name, card_date FROM cards WHERE user_id = ?", [req.user.id], (err, result) => {
-      if (err) console.log(err)
-      res.send(result)
-    })
-  })
-})
+  db.run("INSERT INTO cards (user_id, card_name, card_date) VALUES (?, ?, ?)", [req.user.id, req.body.cardName, req.body.cardDate], (error) => {
+    if (error) console.log(error);
+    console.log("Card added");
+    db.all("SELECT card_id, card_name, card_date FROM cards WHERE user_id = ?", [req.user.id], (error, result) => {
+      if (error) console.log(error);
+      res.send(result);
+    });
+  });
+});
 
 app.post("/addTask", (req, res) => {
-  if (!req.user) return res.send("Please log in")
+  if (!req.user) return res.send("Please log in");
 
-  db.run("INSERT INTO tasks (card_id, task_text) VALUES (?, ?)", [req.body.cardId, req.body.taskText], (err) => {
-    if (err) console.log(err)
-    console.log("Task added")
+  db.run("INSERT INTO tasks (card_id, task_text) VALUES (?, ?)", [req.body.cardId, req.body.taskText], (error) => {
+    if (error) console.log(error);
+    console.log("Task added");
 
-    db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.cardId], (err, result) => {
-      if (err) console.log(err)
-      res.send(result)
-    })
-  })
-})
+    db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.cardId], (error, result) => {
+      if (error) console.log(error);
+      res.send(result);
+    });
+  });
+});
 
 app.get("/getCards", (req, res) => {
-  if (!req.user) return res.send("Please log in")
+  if (!req.user) return res.send("Please log in");
 
-  db.all("SELECT card_id, card_name, card_date FROM cards WHERE user_id = ?", [req.user.id], (err, result) => {
-    if (err) console.log(err)
-    res.send(result)
-  })
-})
+  db.all("SELECT card_id, card_name, card_date FROM cards WHERE user_id = ?", [req.user.id], (error, result) => {
+    if (error) console.log(error);
+    res.send(result);
+  });
+});
 
 app.post("/getTasks", (req, res) => {
-  if (!req.user) return res.send("Please log in")
+  if (!req.user) return res.send("Please log in");
 
-  db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.cardId], (err, result) => {
-    if (err) console.log(err)
-    res.send(result)
-  })
-})
+  db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.cardId], (error, result) => {
+    if (error) console.log(error);
+    res.send(result);
+  });
+});
 
 app.post("/crossTask", (req, res) => {
-  if (!req.user) return res.send("Please log in")
+  if (!req.user) return res.send("Please log in");
 
-  db.run(`UPDATE tasks SET ${req.body.task_day} = ${req.body.set_to} WHERE task_id = ?`, [req.body.task_id], (err) => {
-    if (err) console.log(err)
-    db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.card_id], (err, result) => {
-      if (err) console.log(err)
-      console.log(result)
-      res.send(result)
-    })
-  })
-})
+  db.run(`UPDATE tasks SET ${req.body.task_day} = ${req.body.set_to} WHERE task_id = ?`, [req.body.task_id], (error) => {
+    if (error) console.log(error);
+    db.all("SELECT task_id, task_text, done_day_one, done_day_two, done_day_three, done_day_four FROM tasks WHERE card_id = ?", [req.body.card_id], (error, result) => {
+      if (error) console.log(error);
+      console.log(result);
+      res.send(result);
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running at PORT: 4000");
 });
 
 // Todo:
-// 1. Routes get their own files
-// 2. Catch up on Node.js Architecture
-// 3. Switch database to postgres hosted somewhere
+// 1. MVC
+// 2. Switch to postgres
 // 4. Consider using an ORM
 // 5. Learn to actually create a proper API
+
+// To learn:
+// 1. Node.js architecture for rest apis
+// 2. an orm
