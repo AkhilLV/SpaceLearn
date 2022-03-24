6d8cd069f06065d0f9871eca378062de6e593be5
Akhil <akhillav@gmail.com>
2022-03-24 17:35:00 +0530

    switched to postgres, foreign keys

--- server/db/db.js
+++ server/db/db.js
@@ -1,43 +1,53 @@
-const sqlite3 = require("sqlite3");
+const { Pool } = require("pg");
 
-const dbFilePath = `${__dirname}/users.db`;
-const db = new sqlite3.Database(dbFilePath, (error) => {
-  if (error) {
-    console.log("Could not connect to database", error);
-  } else {
-    console.log("Connected to database");
-  }
+const db = new Pool({
+  user: "akhil",
+  password: "password",
+  database: "test1",
+  host: "localhost",
+  port: 5432,
 });
 
-db.run(`
+const handleError = (error) => {
+  if (error) {
+    console.log(error);
+  }
+};
+
+db.query(`
   CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id BIGSERIAL NOT NULL PRIMARY KEY,
     username TEXT,
     password TEXT
   )
-`);
+`, handleError);
 
-db.run(`
+db.query(`
   CREATE TABLE IF NOT EXISTS cards (
-    card_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
+    card_id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INTEGER REFERENCES users (user_id),
     card_name TEXT,
     card_date DATE
   )
-`);
+`, handleError);
 
 // tasks get their own table. include date IN task. do away with day_one, day_two ...
-db.run(`
+db.query(`
   CREATE TABLE IF NOT EXISTS tasks (
-    task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    card_id INTEGER,
-    task_text TEXT,
-    done_day_one BOOLEAN DEFAULT FALSE,
-    done_day_two BOOLEAN DEFAULT FALSE,
-    done_day_three BOOLEAN DEFAULT FALSE,
-    done_day_four BOOLEAN DEFAULT FALSE
+    task_id BIGSERIAL NOT NULL PRIMARY KEY,
+    card_id INTEGER REFERENCES cards (card_id),
+    task_text TEXT
   )
-`);
+`, handleError);
+
+db.query(`
+  CREATE TABLE IF NOT EXISTS task_status (
+    task_status BIGSERIAL NOT NULL PRIMARY KEY,
+    task_id INTEGER REFERENCES tasks (task_id),
+    date DATE NOT NULL,
+    done BOOLEAN NOT NULL
+  )
+`, handleError);
 
 module.exports = { db };
 
