b1b143d9cf35cccbcef72d7ad6af1f9a70e915c4
Akhil <akhillav@gmail.com>
2022-04-01 22:58:52 +0530

    updated database schema

--- server/db/database.sql
+++ server/db/database.sql
@@ -14,6 +14,12 @@ CREATE TABLE IF NOT EXISTS cards (
   card_name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS card_dates (
+  card_date_id BIGSERIAL NOT NULL PRIMARY KEY,
+  card_id INTEGER NOT NULL REFERENCES cards (card_id),
+  card_date DATE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS tasks (
   task_id BIGSERIAL NOT NULL PRIMARY KEY,
   card_id INTEGER NOT NULL REFERENCES cards (card_id),
@@ -23,6 +29,18 @@ CREATE TABLE IF NOT EXISTS tasks (
 CREATE TABLE IF NOT EXISTS task_status (
   task_status_id BIGSERIAL NOT NULL PRIMARY KEY,
   task_id INTEGER NOT NULL REFERENCES tasks (task_id),
-  task_date DATE NOT NULL,
+  card_date_id INTEGER NOT NULL REFERENCES card_dates (card_date_id),
   task_done BOOLEAN NOT NULL
 );
+
+-- Creates a session store for passport.js/express sessions
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
