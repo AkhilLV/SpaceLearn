4894d0024e386c7f72c2e6ce205bc94bae21c8ef
Akhil <akhillav@gmail.com>
2022-04-02 23:16:28 +0530

    server: /tasks GET POST

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -1,15 +1,17 @@
 const pool = require("../db/db");
 
 module.exports = {
-  get: (req, res) => {
+  get: async (req, res) => {
     if (!req.user) return res.status(400).send({ message: "not_logged_in" });
 
-    console.log(req.params);
+    const tasks = await pool.query("SELECT task_id, task_text FROM tasks WHERE card_id = $1", [req.params.cardId]);
+    res.send(tasks.rows);
   },
-  post: (req, res) => {
+  post: async (req, res) => {
     if (!req.user) return res.status(400).send({ message: "not_logged_in" });
 
-    console.log(req.params);
+    await pool.query("INSERT INTO tasks (card_id, task_text) VALUES ($1, $2)", [req.params.cardId, req.body.taskText]);
+    res.send({ message: "Success" });
   },
   put: (req, res) => {
     if (!req.user) return res.status(400).send({ message: "not_logged_in" });
--- server/routes/Task.js
+++ server/routes/Task.js
@@ -8,9 +8,6 @@ router.get("/", controller.get);
 router.post("/", controller.post);
 
 router.put("/:taskId", controller.put);
-
-// if (urlHasAction) => use controller.cross else, controller.put
-
 router.delete("/:taskId", controller.delete);
 
 module.exports = router;
