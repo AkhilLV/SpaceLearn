fce2c8403b04cae80a4e9f196bde5c43eb583f22
Akhil <akhillav@gmail.com>
2022-06-01 22:09:12 +0530

    express-validator: request validation

--- server/routes/Card.js
+++ server/routes/Card.js
@@ -1,13 +1,53 @@
 const express = require("express");
+const { body, param, validationResult } = require("express-validator");
 
 const router = express.Router();
-
 const controller = require("../controllers/CardController");
 
 router.get("/", controller.getAll);
-router.post("/", controller.post);
 
-router.get("/:cardId", controller.get);
-router.delete("/:cardId", controller.delete);
+router.post(
+  "/",
+  body("cardName").isString(),
+  body("cardDates").isArray({ min: 1 }),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    controller.post(req, res);
+  },
+);
+
+// test if cardId exists
+router.get(
+  "/:cardId",
+  param("cardId").isInt(),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    controller.get(req, res);
+  },
+);
+
+router.delete(
+  "/:cardId",
+  param("cardId").isInt(),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    controller.delete(req, res);
+  },
+);
 
 module.exports = router;
