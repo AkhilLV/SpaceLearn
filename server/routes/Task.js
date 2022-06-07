91d549ff72506fdf1f694e95524c0c6aa7d07763
Akhil <akhillav@gmail.com>
2022-06-07 22:26:50 +0530

    validations: /tasks

--- server/routes/Task.js
+++ server/routes/Task.js
@@ -1,14 +1,92 @@
 const express = require("express");
+const { body, param, validationResult } = require("express-validator");
+
+const ApiError = require("../error/ApiError");
 
 const router = express.Router({ mergeParams: true });
 
 const controller = require("../controllers/TaskController");
 
-router.get("/", controller.get);
-router.post("/", controller.post);
+router.get(
+  "/",
+  param("cardId").isInt(),
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-router.put("/:taskId", controller.updateText);
-router.patch("/:taskId/:cardDateId", controller.updateStatus);
-router.delete("/:taskId", controller.delete);
+    if (!errors.isEmpty()) {
+      next(ApiError.badRequest({ errors: errors.array() }));
+      return;
+    }
+
+    controller.get(req, res);
+  },
+);
+
+router.post(
+  "/",
+  param("cardId").isInt(),
+  body("taskText").isString().isLength({ min: 1 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      next(ApiError.badRequest({ errors: errors.array() }));
+      return;
+    }
+
+    controller.post(req, res);
+  },
+);
+
+router.put(
+  "/:taskId",
+  param("cardId").isInt(),
+  param("taskId").isInt(),
+  body("taskText").isString().isLength({ min: 1 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      next(ApiError.badRequest({ errors: errors.array() }));
+      return;
+    }
+
+    controller.updateText(req, res);
+  },
+);
+
+router.patch(
+  "/:taskId/:cardDateId",
+  param("cardId").isInt(),
+  param("taskId").isInt(),
+  param("cardDateId").isInt(),
+  body("taskDone").isBoolean(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      next(ApiError.badRequest({ errors: errors.array() }));
+      return;
+    }
+
+    controller.updateStatus(req, res);
+  },
+);
+
+router.delete(
+  "/:taskId",
+  param("cardId").isInt(),
+  param("taskId").isInt(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      next(ApiError.badRequest({ errors: errors.array() }));
+      return;
+    }
+
+    controller.delete(req, res);
+  },
+);
 
 module.exports = router;
---
+++ server/test/Tasks.js
@@ -0,0 +1,51 @@
+process.env.NODE_ENV = "test";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Auth", () => {
+  describe("/POST login", () => {
+    it("should log in", (done) => {
+      chai.request(server)
+        .post("/auth/login")
+        .send({
+          username: "test_user",
+          password: "123", // create new user
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+
+    it("should not log in", (done) => {
+      chai.request(server)
+        .post("/auth/login")
+        .send({
+          password: "123232124212",
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it("should not log in", (done) => {
+      chai.request(server)
+        .post("/auth/login")
+        .send({
+          username: "my_non_existent_user",
+          password: "123",
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+  });
+});
