4fd13913252706c5e42b8a62d99f98f8702e8bc7
Akhil <akhillav@gmail.com>
2022-06-12 23:45:39 +0530

    feat: edit task

--- client/src/components/Tasks/Tasks.jsx
+++ client/src/components/Tasks/Tasks.jsx
@@ -1,7 +1,10 @@
-import { useContext } from "react";
-import { getCard, crossTask, deleteTask } from "../../api";
+import { useContext, useState } from "react";
+import { getCard, crossTask, deleteTask, editTask } from "../../api";
 import CardContext from "../../contexts/CardContext";
+import ModalContext from "../../contexts/ModalContext";
+
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
+import Form from "../Form/Form";
 
 import "./Tasks.css";
 
@@ -9,6 +12,11 @@ export default function Tasks({
   tasks, taskDone,
 }) {
   const { selectedCardId, selectedDateId, setCardData } = useContext(CardContext);
+
+  const [selectedTaskId, setSelectedTaskId] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+
+  const { setShowInfoModal } = useContext(ModalContext);
 
   const handleClick = async (e) => {
     if (!(e.target.matches(".clickable"))) return;
@@ -36,9 +44,15 @@ export default function Tasks({
     }
   };
 
-  const handleDelete = async (e) => {
-    const task = e.target.parentNode.parentNode.parentNode; // go up from button to task div
+  const getTaskId = (el) => {
+    const task = el.parentNode.parentNode.parentNode; // go up from button to task div
     const taskId = task.dataset.taskid;
+
+    return taskId;
+  };
+
+  const handleDelete = async (e) => {
+    const taskId = getTaskId(e.target);
 
     try {
       await deleteTask(selectedCardId, taskId);
@@ -50,29 +64,72 @@ export default function Tasks({
     }
   };
 
-  const handleEdit = () => { };
+  const handleEdit = (e, setShowDropdownMenu) => {
+    const taskId = getTaskId(e.target);
+    setSelectedTaskId(taskId);
+
+    setShowDropdownMenu(false);
+    setShowForm(true);
+  };
+
+  const handleEditTaskForm = async (e, inputValues) => {
+    const taskText = inputValues[1];
+
+    if (!taskText) return setShowInfoModal([true, "Fill all fields"]);
+
+    try {
+      await editTask(selectedCardId, selectedTaskId, { taskText });
+
+      const res = await getCard(selectedCardId);
+      setCardData(res.data);
+
+      setShowForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className={`tasks ${taskDone && "completed-tasks"}`} onClick={handleClick}>
-      {tasks.map((task) => (
-        <div key={task.taskId} data-taskid={task.taskId} className="clickable task">
-          <div className="clickable center-vertical">
-            <div className="clickable circle" />
-            <span className="clickable task-text">{task.taskText}</span>
+    <>
+      {showForm && (
+      <Form
+        headerText="Edit task"
+        inputItems={[
+          {
+            id: 1,
+            labelText: "Task Name",
+            inputType: "text",
+            // inputValue: cardData.cardName,
+          },
+        ]}
+        submitBtnText="Edit card"
+        onSubmit={handleEditTaskForm}
+        setShowForm={setShowForm}
+      />
+      )}
+      <div className={`tasks ${taskDone && "completed-tasks"}`} onClick={handleClick}>
+        {tasks.map((task) => (
+          <div key={task.taskId} data-taskid={task.taskId} className="clickable task">
+            <div className="clickable center-vertical">
+              <div className="clickable circle" />
+              <span className="clickable task-text">{task.taskText}</span>
+            </div>
+            <DropdownMenu buttons={[
+              {
+                buttonName: "Delete",
+                handler: handleDelete,
+              },
+              {
+                buttonName: "Edit",
+                handler: handleEdit,
+              },
+            ]}
+            />
           </div>
-          <DropdownMenu buttons={[
-            {
-              buttonName: "Delete",
-              handler: handleDelete,
-            },
-            {
-              buttonName: "Edit",
-              handler: handleEdit,
-            },
-          ]}
-          />
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+    </>
+
   );
 }
