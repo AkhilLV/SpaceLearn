2fa65a03471f502ff0212424c342c64a8c834220
Akhil <akhillav@gmail.com>
2022-08-01 21:10:11 +0530

    changed icon svgs, refactor css

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext, useEffect } from "react";
 
 import "./Sidebar.css";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, NavLink } from "react-router-dom";
 import taskBoard from "../../assets/task-board.svg";
 
 import Form from "../Form/Form";
@@ -10,11 +10,13 @@ import CardListing from "../CardListing/CardListing";
 import addIcon from "../../assets/icons/add.svg";
 import menuIcon from "../../assets/icons/menu.svg";
 import archiveIcon from "../../assets/icons/archive.svg";
+import settingsIcon from "../../assets/icons/settings.svg";
 
 import { getCards, postCard } from "../../api";
 
 import CardContext from "../../contexts/CardContext";
 import ModalContext from "../../contexts/ModalContext";
+
 import generateCardDates from "../../helpers/generateCardDates";
 
 function Sidebar() {
@@ -94,14 +96,18 @@ function Sidebar() {
       )}
 
       <div className="dashboard-menu">
-        <Link to="/dashboard" className="dashboard-link active">
+        <NavLink to="/dashboard" className="dashboard-link">
           <img src={menuIcon} />
           <p>Dashboard</p>
-        </Link>
-        <Link to="/archives" className="dashboard-link">
+        </NavLink>
+        <NavLink to="/archives" className="dashboard-link">
           <img src={archiveIcon} />
           <p>Archived</p>
-        </Link>
+        </NavLink>
+        <NavLink to="/settings" className="dashboard-link">
+          <img src={settingsIcon} />
+          <p>Settings</p>
+        </NavLink>
       </div>
 
       {cards.length ? (
