17ffb7c45c881a2869e760fc95b775a80ba1dc67
Akhil <akhillav@gmail.com>
2022-08-13 21:13:28 +0530

    db env variables

--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,11 @@ const { Pool } = require("pg");
 const isTestDatabase = process.env.NODE_ENV === "test" && true;
 
 const pool = new Pool({
-  user: "akhil",
-  password: "password",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: isTestDatabase ? "space_learn_test" : "space_learn",
-  host: "localhost",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
