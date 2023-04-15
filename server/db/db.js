f41336e6cf254e4a520ffa8fd0c0e7c3407432a1
Akhil <akhillav@gmail.com>
2023-04-15 23:48:40 +0530

    deploy

--- server/db/db.js
+++ server/db/db.js
@@ -2,12 +2,15 @@ const { Pool } = require("pg");
 
 const isTestDatabase = process.env.NODE_ENV === "test" && true;
 
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: isTestDatabase ? "space_learn_test" : "space_learn",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-});
+const connectionString = process.env.NEON_CONNECTION_STRING;
+const pool = new Pool(
+  { connectionString } || {
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: isTestDatabase ? "space_learn_test" : "space_learn",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
 
 module.exports = pool;
